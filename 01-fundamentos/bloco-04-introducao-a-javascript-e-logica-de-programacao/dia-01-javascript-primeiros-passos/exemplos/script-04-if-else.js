3412b8c8a943ae7d94cc73bd9510dfdb1418a058
André <andreaguilar.ti@gmail.com>
2022-07-18 01:35:47 -0300

    exercicios 9, 10 e 11

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exemplos/script-04-if-else.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exemplos/script-04-if-else.js
@@ -1,6 +1,6 @@
 // 1 - Crie uma constante que receba a nota de uma pessoa candidata em um desafio técnico, e atribua a ela um valor entre 1 e 100;
 
-const nota = ;
+const estado = 80;
 
 /* 2 - Implemente uma lógica que verifique se a pessoa candidata foi aprovada, reprovada ou se ela está na lista de espera. Para isso, considere as seguintes informações:
 Se a nota for maior ou igual a 80, imprima "Parabéns, você foi aprovada(o)!"
@@ -13,10 +13,10 @@ Se a nota for menor que 60, imprima "Você foi reprovada(o)" */
 
 // 4 - Altere o valor da nota para verificar se as condições que você implementou funcionam;
 
-if (nota >= 80) {
+if (estado >= 80) {
   console.log("Parabéns, você foi aprovado");
 }
-else if (nota < 80 && nota >= 60) {
+else if (estado < 80 && estado >= 60) {
   console.log("Você está na nossa lista de espera");
 }
 else {
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exemplos/script10-switch-case-avancado.js
@@ -0,0 +1,28 @@
+
+let mes = 'novembr';
+let estacaoDoAno = '?';
+
+switch (mes) {
+    case 'janeiro':
+    case 'fevereiro':
+    case 'março':
+        estacaoDoAno = 'Verão';
+        break;
+    case 'abril':
+    case 'maio':
+    case 'junho':
+        estacaoDoAno = 'Outono';
+        break;
+    case 'julho':
+    case 'agosto':
+    case 'setembro':
+        estacaoDoAno = 'Inverno';
+        break;
+    case 'outubro':
+    case 'novembro':
+    case 'dezembro':
+        estacaoDoAno = 'Primavera';
+        break;
+}
+
+console.log(estacaoDoAno); // 'Outono'
