96d92eacb04abff9a6bc835f2825c05ae8654620
André <andreaguilar.ti@gmail.com>
2022-07-19 17:20:55 -0300

    exercicios e exemplos de for/of

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exemplos-for-of/exemplo-01-for-of-array-de-numeros.js
@@ -0,0 +1,11 @@
+
+let numeros = [1,2,3,4,5];
+for(let numero of numeros) {
+  console.log(numero);
+}
+
+// OU
+
+for (let index = 0; index < numeros.length; index += 1) {
+  console.log(numeros[index]);
+}
