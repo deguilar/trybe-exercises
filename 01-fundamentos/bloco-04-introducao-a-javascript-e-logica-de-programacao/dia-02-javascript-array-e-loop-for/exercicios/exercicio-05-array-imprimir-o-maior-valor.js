037ffe848801330860a2d2c30dcce88bbae48acc
André <andreaguilar.ti@gmail.com>
2022-07-21 00:31:36 -0300

    exercício 5 finalizado

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/exercicio-05-array-imprimir-o-maior-valor.js
@@ -0,0 +1,28 @@
+// 5 - 🚀 Utilizando for, descubra qual o maior valor contido no array e imprima-o;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorValor = 0;
+
+for (index = 0; index < numbers.length; index += 1) {
+ if (numbers[index] > maiorValor) {
+   maiorValor = numbers[index];
+ }
+}
+
+console.log(maiorValor);
+
+// OU (gabarito trybe)
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// o valor da variável higherNumber poderia ser qualquer um, desde que fosse menor que o menor número do array numbers. Caso atribuíssemos o valor 101 para a variável, nosso algoritmo estaria errado, pois ele nunca acharia um número maior que 101 no array. Para resolver esse problema vamos iniciar a variavel com o primeiro valor do array. Dessa forma podemos pular a posição 0 e iniciar com index = 1
+
+// let higherNumber = numbers[0];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   if (numbers[index] > higherNumber) {
+//     higherNumber = numbers[index];
+//   }
+// }
+
+// console.log(higherNumber);
