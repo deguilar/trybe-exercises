0c228ae9601c6b77b5729079c342a1271849afdd
André <andreaguilar.ti@gmail.com>
2022-07-28 14:29:16 -0300

    bônus 3 finalizado e adição do enunciado nos bônus 1 e 2

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus-01-fazer-quadrado-de-asteriscos.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus-01-fazer-quadrado-de-asteriscos.js
@@ -1,3 +1,13 @@
+/* 1 - Agora vamos trabalhar com algumas formas geométricas! Faça um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+Copiar
+n = 5
+
+*****
+*****
+*****
+*****
+***** */
+
 let numero = 4;
 let asterisco = "*";
 let soma = '';
--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus-02-meia-piramide.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus-02-meia-piramide.js
@@ -1,3 +1,13 @@
+/* 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+Copiar
+n = 5
+
+*
+**
+***
+****
+***** */
+
 let numero = 5;
 let asterisco = '*';
 let soma = '';
