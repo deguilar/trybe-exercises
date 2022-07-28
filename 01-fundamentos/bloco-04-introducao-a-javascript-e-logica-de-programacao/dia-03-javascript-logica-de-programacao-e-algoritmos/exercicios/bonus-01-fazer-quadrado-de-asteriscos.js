f795e42a5ab322ad452aae8a2cff78b6ab43cbb0
André <andreaguilar.ti@gmail.com>
2022-07-28 14:21:43 -0300

    bônus 1 finalizado

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/bonus-01-fazer-quadrado-de-asteriscos.js
@@ -0,0 +1,12 @@
+let numero = 4;
+let asterisco = "*";
+let soma = '';
+
+for (let index = 1; index <= numero; index += 1) {
+  soma = soma + asterisco;
+}
+
+for (let index = 1; index <= numero; index += 1) {
+  console.log(soma);
+}
+
