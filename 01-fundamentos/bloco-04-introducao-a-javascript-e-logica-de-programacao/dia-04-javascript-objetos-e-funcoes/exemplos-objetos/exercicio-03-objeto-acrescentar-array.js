8bd482cf48363067a57a8d06b36cdfedc825a8e7
André <andreaguilar.ti@gmail.com>
2022-07-29 16:26:08 -0300

    exercício 4 finalizado

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exemplos-objetos/exercicio-03-objeto-acrescentar-array.js
@@ -0,0 +1,17 @@
+/* 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+[2006, 2007, 2008, 2009, 2010, 2018] */
+
+let person = {
+  name: 'Marta',
+  lastName: 'Silva',
+  age: 34,
+  medals: {
+    golden: 2,
+    silver: 3
+  },
+};
+
+person['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
+
+console.log(person);
