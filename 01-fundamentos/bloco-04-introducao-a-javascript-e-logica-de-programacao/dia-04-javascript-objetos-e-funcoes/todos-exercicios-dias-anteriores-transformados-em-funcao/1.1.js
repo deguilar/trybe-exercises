3532884bfa6a4976be4af24f2a6d5c0824a28f96
André <andreaguilar.ti@gmail.com>
2022-08-30 15:03:16 -0300

    exercicios e exemplos

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/todos-exercicios-dias-anteriores-transformados-em-funcao/1.1.js
@@ -0,0 +1,25 @@
+function adicao(a, b) {
+  return a + b;
+}
+
+function subtracao(a, b) {
+  return a - b;
+}
+
+function multiplicacao(a, b) {
+  return a * b;
+}
+
+function divisao(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
+console.log(adicao(45, 15));
+console.log(subtracao(45, 15));
+console.log(multiplicacao(45, 15));
+console.log(divisao(45, 15));
+console.log(modulo(45, 15));
