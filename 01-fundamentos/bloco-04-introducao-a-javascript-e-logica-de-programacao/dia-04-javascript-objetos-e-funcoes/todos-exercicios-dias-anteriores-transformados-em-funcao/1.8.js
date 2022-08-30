0af134846b2c7d8adc1ebce0c032c6579b6c910e
André <andreaguilar.ti@gmail.com>
2022-08-30 16:41:37 -0300

    exercicio 1.8

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/todos-exercicios-dias-anteriores-transformados-em-funcao/1.8.js
@@ -0,0 +1,9 @@
+function parOuImpar(a, b, c) {
+  if (((a % 2) === 0) || ((b % 2) === 0) || ((c % 2) === 0)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(parOuImpar(41, 88, 121));
